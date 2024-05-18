48aaa4544c93859b04663081873120a805c3c0a5
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-05-18 00:48:39 -0600

    Update chat_assistant.rb

--- chat_assistant.rb
+++ chat_assistant.rb
@@ -2,3 +2,8 @@ require 'openai'
 
 OpenAI.api_key = "MY_API_KEY"
 
+messages = []
+system_msg = "Write Prompt"
+messages << {role: "system", content: system_msg}
+
+puts "Your new assistant is ready!"
